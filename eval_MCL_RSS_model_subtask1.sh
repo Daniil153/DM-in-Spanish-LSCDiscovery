4c01f707cdf85e8ef278130e350c2e3f80c785a4
Daniil Khomskii <homdanil123@gmail.com>
2022-05-19 01:41:05 +0300

    add eval scrs

---
+++ eval_MCL_RSS_model_subtask1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd mcl-wic
+ckpt=../WIC_RSS/pytorch_model.bin
+ckpt_path=$(dirname $ckpt)
+part=../pairs_sub1
+python -u run_model.py --max_seq_len=500 --do_eval --ckpt_path $ckpt_path --eval_input_dir $part/ \
+  --eval_output_dir $ckpt_path/scores/$part/ --output_dir $ckpt_path
--- eval_best_eval_model.sh
+++ eval_best_eval_model.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-mkdir -p for_gold
-cp -r rushiftEval/*rusemshift for_gold/
-python src/tsv_to_json_gold.py --path_to_combine for_gold/
-python src/sample_scr.py --path_to_test rushiftEval/Evaluation_words/ --path_to_dev rushiftEval/RuSemShift_instances/ --path_to_sampled pairs/ --path_to_combine pairs/combine/ --in_words_dev1 rushiftEval/testset1.tsv --in_words_dev2 rushiftEval/testset2.tsv --delete_short_and_long True --delete_first_and_last_position True --n_pairs 100
-echo "sample comlete"
 cd mcl-wic
-python run_model.py --do_eval --ckpt_path ../first_concat --eval_input_dir ../pairs/combine/sampled_data --eval_output_dir rusemshift_predictions/ --output_dir ../first_concat --loss mse_loss --pool_type first --symmetric true --train_scd 
+python run_model.py --do_eval --ckpt_path ../WIC_RSS --eval_input_dir ../pairs_sub1 --eval_output_dir rusemshift_predictions/ --output_dir ../WIC_RSS --loss crossentropy_loss --pool_type mean --symmetric true --train_scd 
 cd ..
 mkdir -p sampled_data
 mkdir -p sampled_data/score
