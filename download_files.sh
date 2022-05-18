d9025b7695e839297c2519acb4aa1dbb32e611e4
Daniil Khomskii <homdanil123@gmail.com>
2022-05-19 02:38:56 +0300

    fix data

--- download_files.sh
+++ download_files.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://zenodo.org/record/6562394/files/pairs_sub1.zip
+wget https://zenodo.org/record/6562408/files/pairs_sub1.zip
 wget https://zenodo.org/record/6562349/files/pairs_sub2.zip
 unzip pairs_sub1.zip
 rm pairs_sub1.zip
