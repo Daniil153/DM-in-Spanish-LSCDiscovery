9eb86188a7c78f9d5198e1f281a2c19d4a49a7ca
Daniil Khomskii <homdanil123@gmail.com>
2022-05-19 12:23:46 +0300

    add srcs

--- eval_WIC+RSS+DWUG+XLWSD_model_subtask2.sh
+++ eval_WIC+RSS+DWUG+XLWSD_model_subtask2.sh
@@ -5,3 +5,5 @@ ckpt_path=$(dirname $ckpt)
 part=../pairs_sub2
 python -u run_model.py --max_seq_len=500 --do_eval --ckpt_path $ckpt_path --eval_input_dir $part/ \
   --eval_output_dir $ckpt_path/scores/$part/ --output_dir $ckpt_path
+cd ..
+python src/agg_scores_sub2.py WIC+RSS+DWUG+XLWSD/pairs_sub2
--- eval_WIC_RSS_model_subtask2.sh
+++ eval_WIC_RSS_model_subtask2.sh
@@ -2,6 +2,8 @@
 cd mcl-wic
 ckpt=../WIC_RSS/pytorch_model.bin
 ckpt_path=$(dirname $ckpt)
-part=../pairs_sub1
+part=../pairs_sub2
 python -u run_model.py --max_seq_len=500 --do_eval --ckpt_path $ckpt_path --eval_input_dir $part/ \
   --eval_output_dir $ckpt_path/scores/$part/ --output_dir $ckpt_path
+cd ..
+python src/agg_scores_sub2.py WIC_RSS/pairs_sub2
