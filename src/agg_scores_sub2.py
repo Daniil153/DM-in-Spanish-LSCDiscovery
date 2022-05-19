fa0ac08433ef0f9d97a93caf582632192e9c25d7
Daniil Khomskii <homdanil123@gmail.com>
2022-05-19 12:15:25 +0300

    add 2subtask apd

--- src/agg_scores_sub2.py
+++ src/agg_scores_sub2.py
@@ -25,15 +25,14 @@ def submission_subtask1(path_to_scores):
          scores.append(np.mean(list(df.agg_score)))
          temp_word = df.iloc[0]['id'].split('.')[1]
          words.append(temp_word)
-    f = open("population_restricted.txt")
-    tg_words = f.read().split('\n')[:-1]
-    missed_words = list(set(tg_words).symmetric_difference(set(words)))
-    words.extend(missed_words)
-    scores.extend([0.5] * len(missed_words))
     df_scores['word'] = words
     df_scores['change_graded'] = scores
     df_scores['COMPARE'] = scores
-    df_scores.to_csv(f"{path_to_model}/subtask1_submission_apd.tsv", sep='\t')
+    binary_scores = list(df_scores['change_graded'].apply(lambda r: 1 if r >= 0.5 else 0))
+    df_scores['change_binary'] = binary_scores
+    df_scores['change_binary_gain'] = binary_scores
+    df_scores['change_binary_loss'] = binary_scores
+    df_scores.to_csv(f"{path_to_model}/subtask2_submission_apd.tsv", sep='\t')
 
 if __name__ == '__main__':
     fire.Fire(submission_subtask1)
