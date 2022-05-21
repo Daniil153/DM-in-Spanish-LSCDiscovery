ebf51a537419defda4c67cd8add2f162c4c4f12f
Daniil Khomskii <homdanil123@gmail.com>
2022-05-21 19:18:50 +0300

    add

--- eval_WIC+RSS+DWUG+XLWSD_model_subtask1.sh
+++ eval_WIC+RSS+DWUG+XLWSD_model_subtask1.sh
@@ -6,4 +6,4 @@ part=../pairs_sub1
 python -u run_model.py --max_seq_len=500 --do_eval --ckpt_path $ckpt_path --eval_input_dir $part/ \
   --eval_output_dir $ckpt_path/scores/$part/ --output_dir $ckpt_path
 cd ..
-python src/agg_scores_sub1.py WIC+RSS+DWUG+XLWSD/pairs_sub1
+python src/agg_scores_sub1_apd.py WIC+RSS+DWUG+XLWSD/pairs_sub1 pairs_sub1
--- eval_WIC_DWUG+XLWSD_model_subtask2.sh
+++ eval_WIC_DWUG+XLWSD_model_subtask2.sh
@@ -6,4 +6,4 @@ part=../pairs_sub2
 python -u run_model.py --max_seq_len=500 --do_eval --ckpt_path $ckpt_path --eval_input_dir $part/ \
   --eval_output_dir $ckpt_path/scores/$part/ --output_dir $ckpt_path
 cd ..
-python src/agg_scores_sub2.py WIC_DWUG+XLWSD/pairs_sub2
+python src/agg_scores_sub2_apd.py WIC_DWUG+XLWSD/pairs_sub2 pairs_sub2
